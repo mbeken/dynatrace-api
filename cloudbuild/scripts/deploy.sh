e3d7ac63c32af436a3ae0dc9902f5d19361347f9
Matt Beken <=>
2020-08-27 13:10:21 -0600

    troubleshoot varioable substituion

--- cloudbuild/scripts/deploy.sh
+++ cloudbuild/scripts/deploy.sh
@@ -1,3 +1,4 @@
 code_version=$(python setup.py --version) && \
 pip_version=$(yolk -V dynatrace_api | cut -d" " -f2) && \
+echo $_PYPI_ENV && \
 ([ "$code_version" != "$pip_version" ] && twine upload --repository $_PYPI_ENV dist/* -u $_PYPI_USERNAME -p $(cat decrypted-data.txt)  || echo "No new version")
