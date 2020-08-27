912efc5565e90f40af5ed1ec649059d33a4aef1d
Matt Beken <=>
2020-08-27 12:48:53 -0600

    add in deploy step and parameterize pw

--- cloudbuild/scripts/deploy.sh
+++ cloudbuild/scripts/deploy.sh
@@ -1,3 +1,3 @@
 code_version=$(python setup.py --version) && \
 pip_version=$(yolk -V dynatrace_api | cut -d" " -f2) && \
-([ "$code_version" != "$pip_version" ] && twine upload dist/* -u $_PYPI_USERNAME -p $PYPI_PASSWORD || echo "No new version")
+([ "$code_version" != "$pip_version" ] && twine upload dist/* -u $_PYPI_USERNAME -p $(cat pypi_test_pw.txt) || echo "No new version")
