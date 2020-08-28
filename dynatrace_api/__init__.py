c2041525797a34419aad7a3e6b1f66ce3ee57373
Matt Beken <=>
2020-08-28 08:18:38 -0600

    add linting to pipeline

--- dynatrace_api/__init__.py
+++ dynatrace_api/__init__.py
@@ -1,7 +1,23 @@
-"""dynatrace_api
-
-Module for accessing the Dynatrace Environment and Configuration APIs
-
+"""[   summary]    
 """
 
 __version__ = '0.0.2'
+
+
+def doc_test(something, optional=None):
+    """[summary]
+
+    Args:
+        something ([type]): [description]
+        optional ([type], optional): [description]. Defaults to None.
+
+    Raises:
+        Exception: [description]
+
+    Returns:
+        [type]: [description]
+    """
+    if something:
+        return True
+    else:
+        raise Exception
